717f0c4ba286874ef50a220e93b7c62e8b89d4ca
Tamas Cservenak <tamas@cservenak.net>
2014-09-10 17:05:54 +0200

    Deprecated and reformat, no code change

--- src/main/java/org/sonatype/spice/zapper/internal/Check.java
+++ src/main/java/org/sonatype/spice/zapper/internal/Check.java
@@ -1,34 +1,32 @@
 package org.sonatype.spice.zapper.internal;
 
+/**
+ * @deprecated Use Guava Preconditions instead.
+ */
+@Deprecated
 public class Check
 {
-    public static <T> T notNull( final T t, final Class<?> clazz )
-    {
-        return notNull( t, new Template( "%s is null!", clazz.getSimpleName() ) );
+  public static <T> T notNull(final T t, final Class<?> clazz) {
+    return notNull(t, new Template("%s is null!", clazz.getSimpleName()));
+  }
+
+  public static <T> T notNull(final T t, final Object message) {
+    if (null == t) {
+      throw new NullPointerException(String.valueOf(message));
     }
 
-    public static <T> T notNull( final T t, final Object message )
-    {
-        if ( null == t )
-        {
-            throw new NullPointerException( String.valueOf( message ) );
-        }
+    return t;
+  }
 
-        return t;
+  public static void argument(boolean condition, final Object message) {
+    argument(condition, null, message);
+  }
+
+  public static <T> T argument(boolean condition, final T t, final Object message) {
+    if (!condition) {
+      throw new IllegalArgumentException(String.valueOf(message));
     }
 
-    public static void argument( boolean condition, final Object message )
-    {
-        argument( condition, null, message );
-    }
-
-    public static <T> T argument( boolean condition, final T t, final Object message )
-    {
-        if ( !condition )
-        {
-            throw new IllegalArgumentException( String.valueOf( message ) );
-        }
-
-        return t;
-    }
+    return t;
+  }
 }
